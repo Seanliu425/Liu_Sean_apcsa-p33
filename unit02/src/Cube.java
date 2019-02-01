cd0b377753f9ff5793a2424d6bddd1b6ac3e5a3a
Sean Liu <seanliu0425@gmail.com>
2019-02-01 13:14:26 -0800

    Unit 03 push

---
+++ unit02/src/Cube.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		//(6 * side)**2;
+		//return 0;
+		return 6*side*side;
+	}
+}
