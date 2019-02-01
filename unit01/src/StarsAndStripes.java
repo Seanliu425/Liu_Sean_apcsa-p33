43e296e45f717d3f5978ba696492666b20dc5e4d
Sean Liu <seanliu0425@gmail.com>
2019-02-01 12:38:51 -0800

    Unit 01 Push

---
+++ unit01/src/StarsAndStripes.java
@@ -0,0 +1,43 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+     // printTwentyStars();
+   }
+
+   public void printTwentyStars()
+   {
+	   out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println("      ");
+	   out.println("       ");
+   }
+   
+   public void printASmallBox()
+   {	
+   }
+ 
+   public void printABigBox()
+   { 	
+   }   
+}
