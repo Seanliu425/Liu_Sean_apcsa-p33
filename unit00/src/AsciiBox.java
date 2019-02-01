9e05e5943fed39d697d0bebaefece71f5c02705e
Sean Liu <seanliu0425@gmail.com>
2019-02-01 12:36:02 -0800

    Added Unit 00

---
+++ unit00/src/AsciiBox.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name Sean Liu \t  date 1/31/19 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+                       + " );
+		System.out.println("+++++++++++++++++++++++++ " );
+	}
+}
